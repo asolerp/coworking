00578738a4b9672edfa6643870cfce525201784a
Alberto Soler <asoler@topdoctors.es>
2020-10-28 11:27:34 +0100

    Updated deploy

--- i18n.js
+++ i18n.js
@@ -1,18 +1,9 @@
-const NextI18Next = require('next-i18next').default;
+const path = require('path')
+const NextI18Next = require('next-i18next').default
+const { localeSubpaths } = require('next/config').default().publicRuntimeConfig
 
-const NextI18NextInstance = new NextI18Next({
-	defaultLanguage: 'es',
-	otherLanguages: ['en'],
-	localePath: "public/static/locales",
-});
-
-const {
-	appWithTranslation,
-	withTranslation,
-} = NextI18NextInstance;
-
-module.exports = {
-	nextI18next: NextI18NextInstance,
-	appWithTranslation,
-	withTranslation,
-};
+module.exports = new NextI18Next({
+  otherLanguages: ['de'],
+  localeSubpaths,
+	localePath: path.resolve('./public/static/locales'),
+})
